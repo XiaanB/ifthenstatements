0c4a147d9dcafd6441ed5d9e2d732773169c111f
xiaan <xiaan_b@hotmail.com>
2022-03-04 12:18:31 +1300

    && if

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -8,5 +8,15 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("I am scared of aliens");
         }
+
+        int topScore = 80;
+        if (topScore == 100) {
+            System.out.println("you have the high score!");
+        }
+
+        int secondTopScore = 80;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("greater than second top score and less than 100");
+        }
     }
 }
