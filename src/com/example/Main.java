f8d1373fcdb4426d20ec2945136a1da67d934ac2
xiaan <xiaan_b@hotmail.com>
2022-03-04 12:09:31 +1300

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if (isAlien == false)
+            System.out.println("Is not an alien!");
+        
+    }
+}
