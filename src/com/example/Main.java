bfc3a173babc6d41294cbec5790608389780fe8b
xiaan <xiaan_b@hotmail.com>
2022-03-04 12:13:48 +1300

    code blocks

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         boolean isAlien = false;
-        if (isAlien == false)
-            System.out.println("Is not an alien!");
-        
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("I am scared of aliens");
+        }
     }
 }
